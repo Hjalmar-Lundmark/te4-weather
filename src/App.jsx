9099cf5a998b81046789cad134a40fbadcb3260a
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-09-27 13:40:07 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Spinner from './components/Spinner'
+import Footer from './components/Footer'
 
 function App() {
   const [lat, setLat] = useState({})
@@ -25,7 +26,6 @@ function App() {
       });
 
       console.log(lat, long)
-      console.log(typeof (lat), typeof (long))
       if (typeof (lat) === 'object' || typeof (long) === 'object') {
         console.log('lat or long is 0')
         setWeatherData([])
@@ -55,81 +55,85 @@ function App() {
   }, [lat, long])
 
   return (
-    <main className='autoWidth'>
-      <h1>Darth Väder</h1>
-      {weatherData.main ? (
-        <div>
-          <h2>City: {weatherData.name}</h2>
-          <h2>Temperature: {(weatherData.main.temp - 273.15).toFixed(2) + ' C°'}</h2>
-          <h2>Feels like: {(weatherData.main.feels_like - 273.15).toFixed(2) + ' C°'}</h2>
-          <h2>Weather: {weatherData.weather[0].description}</h2>
-          {weatherData.rain ? (
-            <h2>Rain: {weatherData.rain["1h"] + ' mm/h'}</h2>
-          ) : (
-            <></>
-          )}
-          {weatherData.weather[0].icon ? (
-            <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={weatherData.weather[0].description} />
-          ) : (
-            <></>
-          )}
-          <h2>Clouds: {weatherData.clouds.all + '%'} of the sky covered, at the height of {weatherData.cod} m</h2>
-          <h2>Pressure: {weatherData.main.pressure} hPa</h2>
-          <h2>Humidity: {weatherData.main.humidity + '%'}</h2>
-          <h2>Wind:
-            {/* Shows wind speed as a word instead of a number */}
-            {/* I used these for wind names and speeds: https://www.weather.gov/mfl/beaufort https://www.smhi.se/kunskapsbanken/meteorologi/vind/skalor-for-vindhastighet-1.252 */}
-            {weatherData.wind.speed <= 0.2 ? (
-              <> Calm </>
-            ) : weatherData.wind.speed > 0.2 && weatherData.wind.speed <= 3.3 ? (
-              <> Light Breeze </>
-            ) : weatherData.wind.speed > 3.3 && weatherData.wind.speed <= 7.9 ? (
-              <> Moderate Breeze </>
-            ) : weatherData.wind.speed > 7.9 && weatherData.wind.speed <= 13.8 ? (
-              <> Strong Breeze </>
-            ) : weatherData.wind.speed > 13.8 && weatherData.wind.speed <= 24.4 ? (
-              <> Severe gale </>
-            ) : weatherData.wind.speed > 24.4 && weatherData.wind.speed <= 32.6 ? (
-              <> Storm </>
-            ) : weatherData.wind.speed > 32.6 ? (
-              <> Hurricane </>
+    <>
+      <main className='autoWidth'>
+        <h1>Darth Väder</h1>
+        {weatherData.main ? (
+          <div className='grid'>
+            <h2>City: {weatherData.name}</h2>
+            <h2>Temperature: {(weatherData.main.temp - 273.15).toFixed(2) + ' C°'}</h2>
+            <h2>Feels like: {(weatherData.main.feels_like - 273.15).toFixed(2) + ' C°'}</h2>
+            <h2>Weather: {weatherData.weather[0].description}
+              {weatherData.weather[0].icon ? (
+                <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={weatherData.weather[0].description} />
+              ) : (
+                <></>
+              )}
+            </h2>
+            {weatherData.rain ? (
+              <h2>Rain: {weatherData.rain["1h"] + ' mm/h'}</h2>
             ) : (
-              <> Error (wind speed not found)</>
+              <></>
             )}
-            ({weatherData.wind.speed + ' m/s'}) from
-            {/* Shows wind direction as a word instead of a number */}
-            {/* I think this works, maybe I'll add southwest, northeast, etc */}
-            {weatherData.wind.deg >= 135 && weatherData.wind.deg < 225 ? (
-              <> South</>
-            ) : weatherData.wind.deg >= 225 && weatherData.wind.deg < 315 ? (
-              <> West</>
-            ) : weatherData.wind.deg >= 315 && weatherData.wind.deg < 360 || weatherData.wind.deg >= 0 && weatherData.wind.deg < 45 ? (
-              <> North</>
-            ) : weatherData.wind.deg >= 45 && weatherData.wind.deg < 135 ? (
-              <> East</>
+            <h2>Clouds: {weatherData.clouds.all + '%'} of the sky covered, at the height of {weatherData.cod} m</h2>
+            <h2>Pressure: {weatherData.main.pressure} hPa</h2>
+            <h2>Humidity: {weatherData.main.humidity + '%'}</h2>
+            <h2>Wind:
+              {/* Shows wind speed as a word instead of a number */}
+              {/* I used these for wind names and speeds: https://www.weather.gov/mfl/beaufort https://www.smhi.se/kunskapsbanken/meteorologi/vind/skalor-for-vindhastighet-1.252 */}
+              {weatherData.wind.speed <= 0.2 ? (
+                <> Calm </>
+              ) : weatherData.wind.speed > 0.2 && weatherData.wind.speed <= 3.3 ? (
+                <> Light Breeze </>
+              ) : weatherData.wind.speed > 3.3 && weatherData.wind.speed <= 7.9 ? (
+                <> Moderate Breeze </>
+              ) : weatherData.wind.speed > 7.9 && weatherData.wind.speed <= 13.8 ? (
+                <> Strong Breeze </>
+              ) : weatherData.wind.speed > 13.8 && weatherData.wind.speed <= 24.4 ? (
+                <> Severe gale </>
+              ) : weatherData.wind.speed > 24.4 && weatherData.wind.speed <= 32.6 ? (
+                <> Storm </>
+              ) : weatherData.wind.speed > 32.6 ? (
+                <> Hurricane </>
+              ) : (
+                <> Error (wind speed not found)</>
+              )}
+              ({weatherData.wind.speed + ' m/s'}) from
+              {/* Shows wind direction as a word instead of a number */}
+              {/* I think this works, maybe I'll add southwest, northeast, etc */}
+              {weatherData.wind.deg >= 135 && weatherData.wind.deg < 225 ? (
+                <> South</>
+              ) : weatherData.wind.deg >= 225 && weatherData.wind.deg < 315 ? (
+                <> West</>
+              ) : weatherData.wind.deg >= 315 && weatherData.wind.deg < 360 || weatherData.wind.deg >= 0 && weatherData.wind.deg < 45 ? (
+                <> North</>
+              ) : weatherData.wind.deg >= 45 && weatherData.wind.deg < 135 ? (
+                <> East</>
+              ) : (
+                <> Error (wind direction not found)</>
+              )} ({weatherData.wind.deg + '°'})
+            </h2>
+            <h2>Visibility: {weatherData.visibility} meter</h2>
+            {lat && long ? (
+              <>
+                <h2>Latitude: {lat} Longitude: {long}</h2>
+                <iframe src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d269.27453942029365!2d${long}!3d${lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssv!2sse!4v1695801572532!5m2!1ssv!2sse'`} width="600" height="450" loading="lazy" ></iframe>
+              </>
             ) : (
-              <> Error (wind direction not found)</>
-            )} ({weatherData.wind.deg + '°'})
-          </h2>
-          <h2>Visibility: {weatherData.visibility} meter</h2>
-          {lat && long ? (
+              <h2>Latitude and longitude not found</h2>
+            )}
+          </div>
+        ) :
+          (
             <>
-              <h2>Latitude: {lat} Longitude: {long}</h2>
-              <iframe src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d269.27453942029365!2d${long}!3d${lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssv!2sse!4v1695801572532!5m2!1ssv!2sse'`} width="600" height="450" loading="lazy" ></iframe>
+              <h2>Checking if Darth Väder deems you worthy...</h2>
+              <Spinner />
             </>
-          ) : (
-            <h2>Latitude and longitude not found</h2>
-          )}
-        </div>
-      ) :
-        (
-          <>
-            <h2>Checking if Darth Väder deems you worthy...</h2>
-            <Spinner />
-          </>
-        )
-      }
-    </main>
+          )
+        }
+      </main>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <div className="autoWidth">
+                <p>
+                    Sida skapad av <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> | <a href="https://github.com/Hjalmar-Lundmark/te4-weather">Github repo</a>.
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
