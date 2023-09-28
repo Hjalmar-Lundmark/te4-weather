f0c7446d9c7b4e9eb7c3ad1999f03c3d6afabd76
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-09-28 08:27:24 +0200

    Spelling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer>
             <div className="autoWidth">
                 <p>
-                    Sida skapad av <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> | <a href="https://github.com/Hjalmar-Lundmark/te4-weather">Github repo</a>.
+                    Site created by <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> | <a href="https://github.com/Hjalmar-Lundmark/te4-weather">Github repo</a>.
                 </p>
             </div>
         </footer>
