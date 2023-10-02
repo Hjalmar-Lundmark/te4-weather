177157232e2c60f3f6da82207fa5dc57fd8e7687
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-10-02 08:34:47 +0200

    Fix for netlify

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -179,9 +179,7 @@ function Homepage() {
           </>
         ) :
           (
-            <>
-              <Spinner />
-            </>
+            <Spinner />
           )
         }
       </section>
