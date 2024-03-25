1265af6bc17235e6c27018ee42901b1d6635a632
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-03-25 13:22:56 +0100

    Fixed aria-label

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ function Search(props) {
     return (
         <>
             <div className="search">
-                <input type="text" placeholder='Search' id="search" />
+                <input type="text" placeholder='Search' id="search" aria-label="Search" />
                 <button onClick={() => { props.getPlace() }} >Update</button>
             </div>
         </>
