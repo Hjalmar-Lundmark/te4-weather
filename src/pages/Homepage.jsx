be33d4f2d02c9d9a2ff0c744ed4802a091d1a5bf
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2024-03-25 12:59:25 +0100

    Added a stop for invalid serches

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -92,6 +92,10 @@ function Homepage() {
   }
 
   function getPlace() {
+    if (document.querySelector('input').value === '' || document.querySelector('input').value.toLowerCase() == weatherData.name.toLowerCase()) {
+      return
+    }
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${document.querySelector('input').value}&limit=5&appid=${import.meta.env.VITE_API_KEY}`)
       .then(res => res.json())
       .then(result => {
